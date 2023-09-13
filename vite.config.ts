5c6edd2c6b0ac37e2dee7b68c99d1ccebc3ddad4
SanthaKudu47 <kudu200pipers@gmail.com>
2023-09-14 02:33:55 +0900

    refactor:reverted tsconfig settings

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@assets": path.resolve(__dirname, "./assets"),
-      "@src": path.resolve(__dirname, "./"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@src": path.resolve(__dirname, "src"),
       // "@assets": "/src/assets",
       // "@src": "/src",
     },
