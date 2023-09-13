807081135fbb943c40427effcc83d02ca66e90b2
SanthaKudu47 <kudu200pipers@gmail.com>
2023-09-14 02:20:18 +0900

    refactor:updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve:{
+  resolve: {
     alias: {
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@src": path.resolve(__dirname, "src"),
+      //"@assets": path.resolve(__dirname, "src/assets"),
+      //"@src": path.resolve(__dirname, "src"),
+      "@assets": "/src/assets",
+      "@src": "/src",
     },
-  }
-})
+  },
+});
