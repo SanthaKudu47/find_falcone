8f8fe8056d0d14e72118b1497439fd0ebbafbe02
SanthaKudu47 <kudu200pipers@gmail.com>
2023-09-14 03:00:21 +0900

    refactor:updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@src": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "/assets"),
+      "@src": path.resolve(__dirname, "/"),
       // "@assets": "/src/assets",
       // "@src": "/src",
     },
